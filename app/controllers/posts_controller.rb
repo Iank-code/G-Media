9c9b0a2f7344aa83005dda0993d96d7a37590187
Ian Kamau <kian99564@gmail.com>
2023-09-22 13:43:12 +0300

    File upload implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:text)
+      params.require(:post).permit(:text, :image)
     end
 end
